90f164fc954a06cd2161f8ac2511128cf8e48fff
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-01-16 01:09:04 -0500

    made dynamic header

--- app/structure.go
+++ app/structure.go
@@ -82,8 +82,8 @@ func createDynamicHeader(buf []byte) *DNSHeader {
 	return &DNSHeader{
 		ID:      binary.BigEndian.Uint16(buf[:16]),
 		Flags:   binary.BigEndian.Uint16(buf[16:32]),
-		QDCOUNT: binary.BigEndian.Uint16(buf[32:48]),
-		ANCOUNT: binary.BigEndian.Uint16(buf[48:64]),
+		QDCOUNT: 1,
+		ANCOUNT: 1,
 		NSCOUNT: binary.BigEndian.Uint16(buf[64:80]),
 		ARCOUNT: binary.BigEndian.Uint16(buf[80:96]),
 	}

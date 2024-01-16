4fddbb49f3fe2c669bf64bb3443f8a3b3bf7b112
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-01-16 01:42:03 -0500

    made dynamic header

--- app/structure.go
+++ app/structure.go
@@ -84,7 +84,7 @@ func NewDNSHeader() *DNSHeader {
 
 func createDynamicHeader(buf []byte) *DNSHeader {
 	// buf[16] = 1
-	// fmt.Println("buffer 17", buf[16])
+	fmt.Println("buffer 17", buf[16])
 	return &DNSHeader{
 		ID:      binary.BigEndian.Uint16(buf[:16]),
 		Flags:   binary.BigEndian.Uint16(buf[16:32]),

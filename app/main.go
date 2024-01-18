61f80d13fd1027b8300c60a9a4d0eb0bc2023d4e
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-01-18 00:04:20 -0500

    expecting to pass right now

--- app/main.go
+++ app/main.go
@@ -14,7 +14,7 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	argumentOne := os.Args[0]
+	argumentOne := os.Args[1]
 	fmt.Println("hi arg1", argumentOne)
 
 	if string(argumentOne) == "resolver" {
